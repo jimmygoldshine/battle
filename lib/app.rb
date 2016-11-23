5a4342d35cf11927acc80bf2acca61c177f835a9
James <jamesdix@cwgsy.net>
2016-11-23 16:34:05 +0000

    step 19 complete - redirects

--- lib/app.rb
+++ lib/app.rb
@@ -1,17 +1,26 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
       erb(:index)
   end
 
-post '/names' do
-  p params
-  @p1_name = params[:p1_name]
-  @p2_name = params[:p2_name]
-  erb(:play)
-end
+  post '/names' do
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    p session
+    redirect to('/play')
+  end
 
+  get '/play' do
+    p params
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
+    erb(:play)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
