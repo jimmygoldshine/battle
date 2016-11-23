9a2f96fac4504a2ba67759f9950974a99cdfba23
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-23 16:57:58 +0000

    hitpoints added

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -25,3 +25,16 @@ RSpec.feature "Players enter their names", :type => :feature do
     expect(page).to have_text("John vs Barry")
   end
 end
+
+describe "Displaying hitpoints" do
+  before do
+    visit('/')
+    fill_in('p1_name', with: 'John')
+    fill_in('p2_name', with: 'Barry')
+    click_button('submit')
+  end
+
+  it "should display both hitpoints to screen" do
+      expect(page).to have_content('P2_hitpoints')
+  end
+end
