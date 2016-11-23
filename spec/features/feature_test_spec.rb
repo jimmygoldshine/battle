d47da4ac4a67cbab139c892cad51ba67a7d98a16
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-23 14:11:55 +0000

    small changes

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -4,10 +4,10 @@ require 'spec_helper'
 describe "Player creation" do
   context "when both players enter names" do
     it "submits form" do
-      visit('/players')
-      fill_in('Player 1 name', with: 'John')
-      fill_in('Player 2 name', with: 'Barry')
-      click_button('Submit!')
+      visit('/')
+      fill_in('p1_name', with: 'John')
+      fill_in('p2_name', with: 'Barry')
+      click_button('submit')
     end
   end
 end
@@ -16,12 +16,12 @@ end
 
 RSpec.feature "Players enter their names", :type => :feature do
   scenario "when two players enters their names" do
-    visit '/players'
+    visit '/'
 
-    fill_in "Player 1 name", :with => "p1 name"
-    fill_in "PLayer 2 name", :with => "p2 name"
-    click_button "Submit!"
+    fill_in "p1_name", :with => "John"
+    fill_in "p2_name", :with => "Barry"
+    click_button "submit"
 
-    expect(page).to have_text("p1 name vs p2 name")
+    expect(page).to have_text("John vs Barry")
   end
 end
