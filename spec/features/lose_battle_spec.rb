8f63451c5740c9be6b6a2122b4429cb354e87bc2
pflarcology <rhrg500@gmail.com>
2016-11-24 16:47:33 +0000

    adds lost message

--- spec/features/lose_battle_spec.rb
+++ spec/features/lose_battle_spec.rb
@@ -1,7 +1,9 @@
-# require 'spec_helper'
-#
-# feature "lose battle confirmation", :type => :feature do
-#
-#   scenario "if player2 reaches 0hp receive battle loss message" do
-#     sign_in_and_p1_attacks
-#
+require 'spec_helper'
+
+feature "lose battle confirmation", :type => :feature do
+
+  scenario "if player2 reaches 0hp receive battle loss message" do
+    sign_in_and_p1_attacks_twice
+    expect(page).to have_content("Louisa has lost")
+  end
+end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -9,3 +9,11 @@ def sign_in_and_p1_attacks
   sign_in_and_play
   visit "/attack"
 end
+
+def sign_in_and_p1_attacks_twice
+  sign_in_and_p1_attacks
+  click_button('Next')
+  visit "/attack"
+  click_button('Next')
+  visit "/attack"
+end
